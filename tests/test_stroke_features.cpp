235127d169eab99eb33f4b10d86113baa9d7edc2
Campbell French <cfrench229@gmail.com>
2026-01-15 11:45:09 -0500

    build: added support for ARM64 building and x86 so program can run on my raspberry Pi. Also made small unit test changes and gitignore edits

--- tests/test_stroke_features.cpp
+++ tests/test_stroke_features.cpp
@@ -3,11 +3,6 @@
 
 TEST(StrokeFeaturesTest, DefaultStrokeScoresZero) {
     StrokeFeatures f{};
-std::cout << "face angle score: " << score_face_angle(f.face_angle_deg) << "\n";
-std::cout << "stability score: " << score_stability(f.ang_vel_stability) << "\n";
-std::cout << "tempo score: " << score_tempo(f.tempo_ratio) << "\n";
-std::cout << "path score: " << score_path(f.path_deviation) << "\n";
-std::cout << "total score: " << total_score(f) << "\n";
 
     EXPECT_DOUBLE_EQ(total_score(f), 0.0);
 }
